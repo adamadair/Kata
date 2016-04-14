d5cc26f221e8c868d64619321972e9132d1b7aae
adamadair@outlook.com <adamadair@outlook.com>
2016-04-13 23:21:17 -0500

    Day 5

--- kata02/ruby/day5.rb
+++ kata02/ruby/day5.rb
@@ -1,14 +1,16 @@
 module Day5
-    def chop(int, array_of_int)
-        lo,hi=1,array_of_int.length
-        while lo < hi do
-            mid = lo + (hi-lo)/2
-            if array_of_int[mid]==int
-                return mid
-            elsif array_of_int[mid] < int
-                lo = mid + 1
-            else
-                hi = mid - 1         
-        end
-        -1
+  def chop(int, array_of_int)
+    lo,hi=0,array_of_int.length
+    while lo < hi do
+      mid = lo + (hi-lo)/2
+      if array_of_int[mid]==int
+        return mid
+      elsif array_of_int[mid] < int
+        lo = mid + 1
+      else
+        hi = mid
+      end         
     end
+    -1
+  end
+end
--- kata02/ruby/test.rb
+++ kata02/ruby/test.rb
@@ -3,6 +3,7 @@ require_relative 'day1'
 require_relative 'day2'
 require_relative 'day3'
 require_relative 'day4'
+require_relative 'day5'
 
 class TestSimpleNumber < Minitest::Test
    include Day1   
@@ -87,6 +88,33 @@ end
 
 class TestDay4 < Minitest::Test
    include Day4
+    def test_chop
+        assert_equal(-1, chop(3, []))
+        assert_equal(-1, chop(3, [1]))
+        assert_equal(0,  chop(1, [1]))
+        #   
+        assert_equal(0,  chop(1, [1, 3, 5]))
+        assert_equal(1,  chop(3, [1, 3, 5]))
+        assert_equal(2,  chop(5, [1, 3, 5]))
+        assert_equal(-1, chop(0, [1, 3, 5]))
+        assert_equal(-1, chop(2, [1, 3, 5]))
+        assert_equal(-1, chop(4, [1, 3, 5]))
+        assert_equal(-1, chop(6, [1, 3, 5]))
+        #
+        assert_equal(0,  chop(1, [1, 3, 5, 7]))
+        assert_equal(1,  chop(3, [1, 3, 5, 7]))
+        assert_equal(2,  chop(5, [1, 3, 5, 7]))
+        assert_equal(3,  chop(7, [1, 3, 5, 7]))
+        assert_equal(-1, chop(0, [1, 3, 5, 7]))
+        assert_equal(-1, chop(2, [1, 3, 5, 7]))         
+        assert_equal(-1, chop(4, [1, 3, 5, 7]))
+        assert_equal(-1, chop(6, [1, 3, 5, 7]))
+        assert_equal(-1, chop(8, [1, 3, 5, 7]))
+    end
+end
+
+class TestDay5 < Minitest::Test
+   include Day5
     def test_chop
         assert_equal(-1, chop(3, []))
         assert_equal(-1, chop(3, [1]))
